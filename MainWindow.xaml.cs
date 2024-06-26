b3011f99569d8d221e07d1b07639eddbcde1fde0
Kajuran <Kajuran@DESKTOP-RMB9U75>
2024-06-26 17:14:05 +0100

    Somehow got it working finally, but an exception always gets thrown when ending the program, shouldnt really be that much of an issue

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Timers;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace MovingShapes
 {
@@ -20,20 +22,38 @@ namespace MovingShapes
     /// </summary>
     public partial class MainWindow : Window
     {
+        Timer t;
+        Ellipse ellipse;
         public MainWindow()
         {
             InitializeComponent();
-            Ellipse ellipse = new Ellipse();
+            ellipse = new Ellipse();
             ellipse.Height = 30; ellipse.Width = 20;
             ellipse.Fill = new SolidColorBrush(Colors.OrangeRed);
             ellipse.RenderTransform = new TranslateTransform(60, 70);
             cnvsMain.Children.Add(ellipse);
             ellipse.LayoutTransform = new RotateTransform(45);
-
             Ellipse circle = new Ellipse();
             circle.Width = 20; circle.Height = 20;
             circle.Stroke = Brushes.Blue;
             cnvsMain.Children.Add(circle);
+            SetTimer();
+        } 
+        
+        private void SetTimer()
+        {
+            t = new Timer(100);
+            t.Elapsed += TimerEvent;
+            t.Enabled = true;
+        }
+        double angle = 0;
+        private void TimerEvent(Object source, ElapsedEventArgs e)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                ellipse.LayoutTransform = new RotateTransform(angle+=10);
+            });
+            
         }
     }
 }
